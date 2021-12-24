3fe2ee21162bd79aadeb2e02202a4e63b3b480bd
KewlShrezhth <96073792+KewlShrezhth@users.noreply.github.com>
2021-12-24 15:03:45 -0600

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WOOD = (186, 140, 99)
+
+CROWN = pygame.transform.scale(pygame.image.load("assets/crown.png"), (45, 25))
